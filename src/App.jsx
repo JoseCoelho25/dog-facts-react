475ea8e1f6d9e117bb8a3e3a5e240207d5153fb7
JoseCoelho25 <zenani87@hotmail.com>
2023-01-17 22:50:01 +0000

    sync

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     const [checked, setChecked] = useState([]);
   
     useEffect(() => {
-      fetch('https://dog-api.kinduff.com/api/facts?number=7')
+      fetch('https://dog-api.kinduff.com/api/facts?number=5')
         .then(response => response.json())
         .then(data => {
           setBreeds(data.facts)
@@ -44,13 +44,14 @@ function App() {
         <h1 className="text-2xl font-bold m-10">Dog Facts App</h1>
         <div className="block max-w-screen-xl m-6">
           {breeds.map((breed, index) => (
-            <div key={index} className="text-xl flex w-full">
+            <div key={index} className="text-xl flex justify-between w-full bg-orange-300 rounded-lg p-3 mb-2">
               {breed}
+              <div className="justify-end flex flex-nowrap rounded-lg ">
               <button onClick={() => handleRemove(index)}
-                className="items-end"
+                className="bg-blue-300 rounded-lg p-2"
               >Remove</button>
               <input
-                    className="items-end"
+                    className="h-1/2 w-5 m-3 ml-2"
                   type="checkbox"
                   checked={checked[index]}
                   onChange={() => {
@@ -61,15 +62,19 @@ function App() {
                       handleChange(index);
                   }}
               />
-  
+                </div>
             </div>
           ))}
         </div>
-        <button onClick={() => setBreeds(breeds.filter(breed => !breedsToRemove.includes(breed)))}>Remove</button>
         <input type="text"
               value={input}
               onChange={e=> setInput(e.target.value)}
+              className="border-2 border-black flex m-4 w-1/2 h-36 pb-24 pl-4"
+              placeholder="Type the breed or details you want to add!"
         />
+        <button onClick={() => setBreeds(breeds.filter(breed => !breedsToRemove.includes(breed)))}
+                className="bg-blue-500 rounded-lg "
+        >Remove</button>
         <button onClick={handleAdd}>Add</button>
       </div>
     );
